3c0e54d7f8c8947285518928249b408bf72fa8df
rektide de la faye <rektide@voodoowarez.com>
2017-12-24 02:05:59 -0500

    Start signal-listen to listen to, initially, all signals a path or path/interface.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import introspect from "./introspect.js"
+import Introspect from "./introspect.js"
 import * as Dbus from "./dbus"
 
 export let defaults= {
@@ -12,7 +12,36 @@ export function debugMode(){
 	process.on( "unhandledRejection", err=> console.error( err))
 }
 
-export async function exec( opts){
+export async function introspect( opts){
+	var
+	  e= await env(),
+	  result= await Introspect( e),
+	  recombinantFpShit= Object.assign({}, e, { result})
+	return recombinantFpShit // this is actually how i like to combine stuff, slowly, declaratively.
+}
+
+export async function main( opts){
+	opts= opts|| {}
+	var filter= opts.execFilter|| defaults.execFilter|| function(i){return i}
+	var fn= opts.fn|| introspect
+	return await fn()
+		.then( filter)
+		.then( console.log.bind( console)) // show the root path
+}
+
+export async function signalListen( path){
+	var
+	  ctx= await exec()
+	  run= Object.keys( ctx).map(async function( key){
+		var
+		  path= ctx[ key],
+		  signals= path.signals
+		//dbus.getInterface( key, path.name)
+		
+	  })
+}
+
+export async function env( opts){
 	function o( a){
 		return opts&& opts[ a]
 	}
@@ -23,22 +52,15 @@ export async function exec( opts){
 	  dbus= o( "dbus")|| Dbus[ busName](), // "session", "system" or "default" or else
 	  path= o( "path")|| process.env[ p+ "PATH"], // undefined aok: underlying `introspect` default is "/" which is great. it recurses through (c.o. all-in-one dependency).
 	  nodes= o( "nodes")|| [], // permits userland a reference to nodes, which will grow through all descendants of the path
-	  params= o( "params")||{
+	  params= o( "params")|| {
+		prefix: p,
 		service,
+		busName,
 		dbus,
 		path,
 		nodes
-	  },
-	  result= await introspect( params),
-	  recombinantFpShit= Object.assign({}, params, { result})
-	return recombinantFpShit // this is actually how i like to combine stuff, slowly, declaratively.
-}
-
-export async function main( opts){
-	var filter= opts&& opts.execFilter|| defaults.execFilter|| function(i){return i}
-	return await exec()
-		.then( filter)
-		.then( console.log.bind( console)) // show the root path
+	  }
+	return params
 }
 
 export default main;
