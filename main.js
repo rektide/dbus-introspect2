2eec0fa9434fb7f9eeec15ba184be6ef8f1b6b3d
rektide de la faye <rektide@voodoowarez.com>
2017-12-23 23:47:49 -0500

    First pass fixes towards executing Introspect.

--- main.js
+++ main.js
@@ -5,34 +5,32 @@ export let defaults= {
 	prefix: "INTROSPECT_",
 	service: "org.freedesktop.DBus",
 	busName: "default"
-	
 }
 
-function exec( opts){
+export async function exec( opts){
 	function o( a){
 		return opts&& opts[ a]
 	}
 	var
-	  p= o('prefix')|| defaults.prefix,
-	  service= process.env[ p+ "SERVICE"]|| process.argv[2]|| defaults.service,
-	  busName= process.env[ p+ "DBUS"]|| defaults.busName,
-	  dbus= Dbus[ busName](), // "session", "system" or "default" or else
-	  path= processe.env[ p+ "PATH"] // undefined aok: underlying `introspect` default is "/" which is great. it recurses through (c.o. all-in-one dependency).
-	  nodes= [], // permits userland a reference to nodes, which will grow through all descendants of the path
-	  params= {
+	  p= o( "prefix")|| defaults.prefix,
+	  service= o( "service")|| process.env[ p+ "SERVICE"]|| process.argv[2]|| defaults.service,
+	  busName= o( "busName")|| process.env[ p+ "DBUS"]|| defaults.busName,
+	  dbus= o( "dbus")|| Dbus[ busName](), // "session", "system" or "default" or else
+	  path= o( "path")|| processe.env[ p+ "PATH"], // undefined aok: underlying `introspect` default is "/" which is great. it recurses through (c.o. all-in-one dependency).
+	  nodes= o( "nodes")|| [], // permits userland a reference to nodes, which will grow through all descendants of the path
+	  params= o( "paams")||{
 		service,
 		dbus,
 		path,
 		nodes
 	  },
-	  result= introspect(params),
-	  recombinantFpShit= result.then( result=> { result, ..params})
+	  result= await introspect( params),
+	  recombinantFpShit= result.then( result=> Object.assign({}, params, { result}))
 	return recombinantFpShit
 }
 
-function main( opts){
-	exec.then( result=> console.log( opts.result)
+export function main( opts){
+	exec.then( result=> console.log( opts.result))
 }
 
 export default main;
-export main;
